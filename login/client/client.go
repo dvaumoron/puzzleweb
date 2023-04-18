2962a72d21cedaac691610fee4cf809fe2c1c500
dvaumoron <dvaumoron@gmail.com>
2023-04-18 18:14:20 +0200

    working

--- login/client/client.go
+++ login/client/client.go
@@ -18,7 +18,6 @@
 package client
 
 import (
-	"context"
 	"errors"
 	"sort"
 	"time"
@@ -49,8 +48,6 @@ func New(serviceAddr string, dialOptions grpc.DialOption, timeOut time.Duration,
 	}
 }
 
-type requestKind func(pb.LoginClient, context.Context, *pb.LoginRequest) (*pb.Response, error)
-
 type sortableContents []*pb.User
 
 func (s sortableContents) Len() int {
@@ -66,18 +63,62 @@ func (s sortableContents) Swap(i, j int) {
 }
 
 func (client loginClient) Verify(login string, password string) (bool, uint64, error) {
-	return client.verifyOrRegister(login, password, verify)
+	salted, err := client.saltService.Salt(login, password)
+	if err != nil {
+		return false, 0, common.LogOriginalError(client.Logger, err, "LoginClient1")
+	}
+
+	conn, err := client.Dial()
+	if err != nil {
+		return false, 0, common.LogOriginalError(client.Logger, err, "LoginClient2")
+	}
+	defer conn.Close()
+
+	ctx, cancel := client.InitContext()
+	defer cancel()
+
+	response, err := pb.NewLoginClient(conn).Verify(ctx, &pb.LoginRequest{Login: login, Salted: salted})
+	if err != nil {
+		return false, 0, common.LogOriginalError(client.Logger, err, "LoginClient3")
+	}
+	return response.Success, response.Id, nil
 }
 
 func (client loginClient) Register(login string, password string) (bool, uint64, error) {
-	return client.verifyOrRegister(login, password, register)
+	strong, err := client.strengthService.Validate(password)
+	if err != nil {
+		return false, 0, err
+	}
+	if !strong {
+		return false, 0, errWeakPassword
+	}
+
+	salted, err := client.saltService.Salt(login, password)
+	if err != nil {
+		return false, 0, common.LogOriginalError(client.Logger, err, "LoginClient4")
+	}
+
+	conn, err := client.Dial()
+	if err != nil {
+		return false, 0, common.LogOriginalError(client.Logger, err, "LoginClient5")
+	}
+	defer conn.Close()
+
+	ctx, cancel := client.InitContext()
+	defer cancel()
+
+	response, err := pb.NewLoginClient(conn).Register(ctx, &pb.LoginRequest{Login: login, Salted: salted})
+	if err != nil {
+		return false, 0, common.LogOriginalError(client.Logger, err, "LoginClient6")
+	}
+	return response.Success, response.Id, nil
 }
 
 // You should remove duplicate id in list
 func (client loginClient) GetUsers(userIds []uint64) (map[uint64]service.User, error) {
 	conn, err := client.Dial()
 	if err != nil {
-		return nil, common.LogOriginalError(client.Logger, err, "LoginClient1")
+		return nil, common.LogOriginalError(client.Logger, err, "LoginClient7")
 	}
 	defer conn.Close()
 
@@ -86,7 +127,7 @@ func (client loginClient) GetUsers(userIds []uint64) (map[uint64]service.User, e
 
 	response, err := pb.NewLoginClient(conn).GetUsers(ctx, &pb.UserIds{Ids: userIds})
 	if err != nil {
-		return nil, common.LogOriginalError(client.Logger, err, "LoginClient2")
+		return nil, common.LogOriginalError(client.Logger, err, "LoginClient8")
 	}
 
 	logins := map[uint64]service.User{}
@@ -99,17 +140,17 @@ func (client loginClient) GetUsers(userIds []uint64) (map[uint64]service.User, e
 func (client loginClient) ChangeLogin(userId uint64, oldLogin string, newLogin string, password string) error {
 	oldSalted, err := client.saltService.Salt(oldLogin, password)
 	if err != nil {
-		return common.LogOriginalError(client.Logger, err, "LoginClient3")
+		return common.LogOriginalError(client.Logger, err, "LoginClient9")
 	}
 
 	newSalted, err := client.saltService.Salt(newLogin, password)
 	if err != nil {
-		return common.LogOriginalError(client.Logger, err, "LoginClient4")
+		return common.LogOriginalError(client.Logger, err, "LoginClient10")
 	}
 
 	conn, err := client.Dial()
 	if err != nil {
-		return common.LogOriginalError(client.Logger, err, "LoginClient5")
+		return common.LogOriginalError(client.Logger, err, "LoginClient11")
 	}
 	defer conn.Close()
 
@@ -120,7 +161,7 @@ func (client loginClient) ChangeLogin(userId uint64, oldLogin string, newLogin s
 		UserId: userId, NewLogin: newLogin, OldSalted: oldSalted, NewSalted: newSalted,
 	})
 	if err != nil {
-		return common.LogOriginalError(client.Logger, err, "LoginClient6")
+		return common.LogOriginalError(client.Logger, err, "LoginClient12")
 	}
 	if !response.Success {
 		return common.ErrUpdate
@@ -139,17 +180,17 @@ func (client loginClient) ChangePassword(userId uint64, login string, oldPasswor
 
 	oldSalted, err := client.saltService.Salt(login, oldPassword)
 	if err != nil {
-		return common.LogOriginalError(client.Logger, err, "LoginClient7")
+		return common.LogOriginalError(client.Logger, err, "LoginClient13")
 	}
 
 	newSalted, err := client.saltService.Salt(login, newPassword)
 	if err != nil {
-		return common.LogOriginalError(client.Logger, err, "LoginClient8")
+		return common.LogOriginalError(client.Logger, err, "LoginClient14")
 	}
 
 	conn, err := client.Dial()
 	if err != nil {
-		return common.LogOriginalError(client.Logger, err, "LoginClient9")
+		return common.LogOriginalError(client.Logger, err, "LoginClient15")
 	}
 	defer conn.Close()
 
@@ -160,7 +201,7 @@ func (client loginClient) ChangePassword(userId uint64, login string, oldPasswor
 		UserId: userId, OldSalted: oldSalted, NewSalted: newSalted,
 	})
 	if err != nil {
-		return common.LogOriginalError(client.Logger, err, "LoginClient10")
+		return common.LogOriginalError(client.Logger, err, "LoginClient16")
 	}
 	if !response.Success {
 		return common.ErrUpdate
@@ -171,7 +212,7 @@ func (client loginClient) ChangePassword(userId uint64, login string, oldPasswor
 func (client loginClient) ListUsers(start uint64, end uint64, filter string) (uint64, []service.User, error) {
 	conn, err := client.Dial()
 	if err != nil {
-		return 0, nil, common.LogOriginalError(client.Logger, err, "LoginClient11")
+		return 0, nil, common.LogOriginalError(client.Logger, err, "LoginClient17")
 	}
 	defer conn.Close()
 
@@ -182,7 +223,7 @@ func (client loginClient) ListUsers(start uint64, end uint64, filter string) (ui
 		Start: start, End: end, Filter: filter,
 	})
 	if err != nil {
-		return 0, nil, common.LogOriginalError(client.Logger, err, "LoginClient12")
+		return 0, nil, common.LogOriginalError(client.Logger, err, "LoginClient18")
 	}
 
 	list := response.List
@@ -198,7 +239,7 @@ func (client loginClient) ListUsers(start uint64, end uint64, filter string) (ui
 func (client loginClient) Delete(userId uint64) error {
 	conn, err := client.Dial()
 	if err != nil {
-		return common.LogOriginalError(client.Logger, err, "LoginClient13")
+		return common.LogOriginalError(client.Logger, err, "LoginClient19")
 	}
 	defer conn.Close()
 
@@ -207,42 +248,12 @@ func (client loginClient) Delete(userId uint64) error {
 
 	response, err := pb.NewLoginClient(conn).Delete(ctx, &pb.UserId{Id: userId})
 	if err != nil {
-		return common.LogOriginalError(client.Logger, err, "LoginClient14")
+		return common.LogOriginalError(client.Logger, err, "LoginClient20")
 	}
 	if !response.Success {
 		return common.ErrUpdate
 	}
 	return nil
-}
-
-func (client loginClient) verifyOrRegister(login string, password string, kind requestKind) (bool, uint64, error) {
-	salted, err := client.saltService.Salt(login, password)
-	if err != nil {
-		return false, 0, common.LogOriginalError(client.Logger, err, "LoginClient15")
-	}
-
-	conn, err := client.Dial()
-	if err != nil {
-		return false, 0, common.LogOriginalError(client.Logger, err, "LoginClient16")
-	}
-	defer conn.Close()
-
-	ctx, cancel := client.InitContext()
-	defer cancel()
-
-	response, err := kind(pb.NewLoginClient(conn), ctx, &pb.LoginRequest{Login: login, Salted: salted})
-	if err != nil {
-		return false, 0, common.LogOriginalError(client.Logger, err, "LoginClient17")
-	}
-	return response.Success, response.Id, nil
-}
-
-func verify(loginClient pb.LoginClient, ctx context.Context, request *pb.LoginRequest) (*pb.Response, error) {
-	return loginClient.Verify(ctx, request)
-}
-
-func register(loginClient pb.LoginClient, ctx context.Context, request *pb.LoginRequest) (*pb.Response, error) {
-	return loginClient.Register(ctx, request)
 }
 
 func convertUser(user *pb.User, dateFormat string) service.User {

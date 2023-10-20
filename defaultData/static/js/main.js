eb46c6731ef39a112cb97d0fac8070433b6f137a
dvaumoron <dvaumoron@gmail.com>
2023-10-20 08:40:41 +0200

    cleaning (duplicate with puzzletest)

--- defaultData/static/js/main.js
+++
@@ -1,6 +0,0 @@
-const go = new Go();
-WebAssembly.instantiateStreaming(fetch("/static/puzzlefront.wasm"), go.importObject).then((result) => {
-    go.run(result.instance);
-
-    initWikiLink();
-});
